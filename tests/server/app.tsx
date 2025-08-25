d5bacca56e1108e9ba0b606d7a8f96bb55565b4a
Susan Cheng <susan.doggie@gmail.com>
2025-08-25 20:51:06 +0800

    Update app.tsx

--- tests/server/app.tsx
+++ tests/server/app.tsx
@@ -26,48 +26,10 @@
 import _ from 'lodash';
 
 import './app.scss';
-import { QuillEditor, Line } from '../../src';
-import { useState } from 'frosty';
 
 export const App = () => {
-  const [value, setValue] = useState<Line[]>([]);
   return (
     <div style={{ display: 'flex', flex: 1 }}>
-      <QuillEditor
-        style={{ flex: 1 }}
-        value={value}
-        onChangeValue={v => setValue(v)}
-        options={{
-          theme: 'snow',
-          modules: {
-            toolbar: [
-              ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-              ['blockquote', 'code-block'],
-              ['link', 'image', 'video', 'formula'],
-
-              [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-              [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
-              [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
-              [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
-              [{ 'direction': 'rtl' }],                         // text direction
-
-              [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-              [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-
-              [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-              [{ 'font': [] }],
-              [{ 'align': [] }],
-
-              ['clean']                                         // remove formatting button
-            ]
-          },
-        }}
-      />
-      <QuillEditor
-        style={{ flex: 1 }}
-        value={value}
-        disabled
-      />
     </div>
   );
 };
